cf3986fc13e3507e3159bf975b7404db7b0acdc9
BrandoAp <avilabrando14@gmail.com>
2025-09-07 15:01:47 -0500

    Initial Commit

---
+++ parciales/parcial_1.py
@@ -0,0 +1,5 @@
+'''
+Se desea implementar un sistema de gestion de parqueo inteligente para monitorear la ocupación
+de los estacinamientos en un centro comercial. El equipo debe simular en python cómo funcionaría el sistema.
+Se debe generar datos simulados y procesarlos con python.
+'''
